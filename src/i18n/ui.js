22512d5904f0b01b3ca48a96ec912ca22a165a5b
Zerre <zerreytug.gll@gmail.com>
2024-11-10 21:13:26 +0100

    trying to make languages available on the website

---
+++ src/i18n/ui.js
@@ -0,0 +1,17 @@
+import en from './en.json';
+import es from './es.json';
+import ca from './ca.json';
+
+export const languages = {
+  en: 'English',
+  es: 'Español',
+  ca: 'Català',
+};
+
+export const defaultLang = 'en';
+
+export const ui = {
+  en,
+  es,
+  ca
+};
