e4a7674b65205a4ee122fd79802d52e8aad8308d
Zerre <zerreytug.gll@gmail.com>
2024-11-13 22:09:26 +0100

    Change Language Implementation

---
+++ src/i18n/ui.ts
@@ -0,0 +1,121 @@
+export const languages = {
+  en: 'English',
+  es: 'Español',
+  ca: 'Català',
+};
+
+export const defaultLang = 'en';
+
+export const ui = {
+  en: {
+    "title": "Portfolio of <SGL> - Web Developer and Programmer",
+    "name":"Sebastian ",
+    "surname":"Gutierrez",
+    "home": "Home",
+    "about": "About",
+    "aboutTitle": "About me",
+    "contactTitle": "Contact",
+    "en": "English",
+    "es": "Spanish",
+    "ca": "Catalan",
+  
+    "heroTitlePart1": "Hi! I'm",
+    "heroTitlePart2": "Sebastian Gutierrez",
+    "heroSubtitle": "A Computer Engineering student",
+  
+    "descriptionAbout1Part1": "Nice to meet you! I'm",
+    "descriptionAbout1Part2": "Sebastian J. Gutierrez Llorca",
+    "descriptionAbout1Part3": ", a student of Computer Engineering. Throughout my career, I have developed a great interest in software development, from application development to",
+    "descriptionAbout1Part4": "web development",
+    "descriptionAbout1Part5": ". I keep myself informed about new developments in the world of technology to apply new ideas and approaches in my projects.",
+  
+    "descriptionAbout2Part1": "I am a",
+    "descriptionAbout2Part2": "proactive and hard-working person",
+    "descriptionAbout2Part3": ", who values",
+    "descriptionAbout2Part4": "teamwork",
+    "descriptionAbout2Part5": "and seeks to collaborate in projects that contribute to both personal and collective growth. Although I have no work experience yet, I am ready to contribute with my skills and keep learning from every challenge that comes my way.",
+  
+    "stackTitle": "Stack",
+    "stackIntro1": "Due to my academic background, I have acquired",
+    "stackIntro2": "knowledge and experience of several languages of which",
+    "stackIntro3": "I highlight the following",
+  
+    "contactDescription1": "Do you have a project or are you just looking to hire?",
+    "contactDescription2": "Let's work together!",
+    "getInTouch": "Get in touch"
+  },
+  es: {
+    "title": "Portfolio de <SGL> - Desarrollador y Programador Web",
+    "name": "Sebastian ",
+    "surname": "Gutierrez",
+    "home": "Inicio",
+    "about": "Sobre mí",
+    "aboutTitle": "Sobre mí",
+    "contactTitle": "Contacto",
+    "en": "Inglés",
+    "es": "Español",
+    "ca": "Catalán",
+  
+    "heroTitlePart1": "¡Hola! Soy",
+    "heroTitlePart2": "Sebastian Gutierrez",
+    "heroSubtitle": "Un estudiante de Ingeniería en Informática",
+  
+    "descriptionAbout1Part1": "Encantado de conocerte! Soy",
+    "descriptionAbout1Part2": "Sebastian J. Gutierrez Llorca",
+    "descriptionAbout1Part3": ", un estudiante de Ingeniería en Informática. A lo largo de mi carrera, he desarrollado un gran interés en el desarrollo de software, desde el desarrollo de aplicaciones hasta",
+    "descriptionAbout1Part4": "desarrollo web",
+    "descriptionAbout1Part5": ". Me mantengo informado sobre nuevos desarrollos en el mundo de la tecnología para aplicar nuevas ideas y enfoques en mis proyectos.",
+  
+    "descriptionAbout2Part1": "Soy una persona",
+    "descriptionAbout2Part2": "proactiva y trabajadora",
+    "descriptionAbout2Part3": ", que valora el",
+    "descriptionAbout2Part4": "trabajo en equipo",
+    "descriptionAbout2Part5": "y busca colaborar en proyectos que contribuyan tanto al crecimiento personal como colectivo. Aunque aún no tengo experiencia laboral, estoy listo para contribuir con mis habilidades y seguir aprendiendo de cada desafío que se me presente.",
+  
+    "stackTitle": "Habilidades Técnicas",
+    "stackIntro1": "Debido a mi formación académica, he adquirido",
+    "stackIntro2": "conocimientos y experiencia en varios lenguajes de los cuales",
+    "stackIntro3": "destaco los siguientes",
+  
+    "contactDescription1": "¿Tienes un proyecto o simplemente estás buscando contratar?",
+    "contactDescription2": "¡Trabajemos juntos!",
+    "getInTouch": "Ponte en contacto"
+  },  
+  ca: {
+      "title": "Portafoli de <SGL> - Desenvolupador i Programador Web",
+      "name": "Sebastian ",
+      "surname": "Gutierrez",
+      "home": "Inici",
+      "about": "Sobre mi",
+      "aboutTitle": "Sobre mi",
+      "contactTitle": "Contacte",
+      "en": "Anglès",
+      "es": "Espanyol",
+      "ca": "Català",
+    
+      "heroTitlePart1": "Hola! Sóc",
+      "heroTitlePart2": "Sebastian Gutierrez",
+      "heroSubtitle": "Un estudiant d'Enginyeria Informàtica",
+    
+      "descriptionAbout1Part1": "Encantat de conèixer-te! Sóc",
+      "descriptionAbout1Part2": "Sebastian J. Gutierrez Llorca",
+      "descriptionAbout1Part3": ", un estudiant d'Enginyeria Informàtica. Al llarg de la meva carrera, he desenvolupat un gran interès pel desenvolupament de projectes, des del desenvolupament d'aplicacions fins al",
+      "descriptionAbout1Part4": "desenvolupament web",
+      "descriptionAbout1Part5": ". Em mantinc informat sobre els nous desenvolupaments en el món de la tecnologia per aplicar noves idees i enfocaments en els meus projectes.",
+    
+      "descriptionAbout2Part1": "Sóc una persona",
+      "descriptionAbout2Part2": "proactiva i treballadora",
+      "descriptionAbout2Part3": ", que valora el",
+      "descriptionAbout2Part4": "treball en equip",
+      "descriptionAbout2Part5": "i busca col·laborar en projectes que contribueixin tant al creixement personal com col·lectiu. Tot i que encara no tinc experiència laboral, estic preparat per contribuir amb les meves habilitats i continuar aprenent de cada repte que se'm presenti.",
+    
+      "stackTitle": "Habilitats Tècniques",
+      "stackIntro1": "Gràcies a la meva formació acadèmica, he adquirit",
+      "stackIntro2": "coneixements i experiència en diversos llenguatges dels quals",
+      "stackIntro3": "destaco els següents",
+    
+      "contactDescription1": "Tens un projecte o simplement estàs buscant contractar?",
+      "contactDescription2": "Treballem junts!",
+      "getInTouch": "Posa't en contacte"
+    },
+} as const;
